b43094846bb3d39eaeeb274d378ed52164945237
unknown <gunapriya07>
2024-02-14 11:58:40 +0530

    redux

---
+++ redux/src/redux/Action.js
@@ -0,0 +1,8 @@
+import { DECREMENT,INCREMENT } from "./ActionType";
+
+export const incrementLike=()=>{
+    return {type: INCREMENT}
+}
+export const decrementLike=()=>{
+    return {type: DECREMENT}
+}
